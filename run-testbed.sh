1a6caa18f6f2cf684c5e1d5c65304b8e61e99b0b
Özcan Karaca <oezcan.karaca@stud.tu-darmstadt.de>
2024-02-27 06:14:17 +0000

    new outputs

--- run-testbed.sh
+++ run-testbed.sh
@@ -4,8 +4,8 @@ printf "\nStarting Testbed.\n\n"
 
 # run-testbed-automation.sh
 export p2p_algorithm_used_values="false true"  # Sets an environment variable indicating if super peers are used
-export number_of_peers_values="50"   # Sets an environment variable for the number of peers
-export choice_of_pdf_mb_values="32"  # Sets an environment variable with different PDF file sizes
+export number_of_peers_values="20 35 50"   # Sets an environment variable for the number of peers
+export choice_of_pdf_mb_values="2 4 8 16 32 64 128"  # Sets an environment variable with different PDF file sizes
 
 # Base directory path where all project-related files are located
 export BASE_PATH="$HOME/Desktop"  # Sets an environment variable for the base path of project files
